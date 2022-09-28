c7458c1de53ccb7068f993518fc833bd8247bc70
ethan-hann <36464732+ethan-hann@users.noreply.github.com>
2022-09-28 11:31:40 -0500

    Initial Project Files

---
+++ MainForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+
+namespace ChromiumWindow
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(0, 0);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(800, 450);
+            this.webView.Source = new System.Uri("https://localhost", System.UriKind.Absolute);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Web View";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+
+        #endregion
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChromiumWindow.Properties;
+using Microsoft.Web.WebView2.Core;
+
+namespace ChromiumWindow
+{
+    public partial class MainForm : Form
+    {
+        private readonly string[] _arguments;
+        public MainForm(string[] args)
+        {
+            InitializeComponent();
+            _arguments = args;
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            //Parse arguments
+            ParseArguments();
+            
+            //Ensure Web View is ready before we do to much with it.
+            webView.CoreWebView2InitializationCompleted += WebViewOnCoreWebView2InitializationCompleted;
+            await InitializeWebView();
+        }
+
+        private void WebViewOnCoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            if (e.IsSuccess)
+                Console.WriteLine("WebView_CoreWebView2 Initialization Complete");
+        }
+
+        private async Task InitializeWebView()
+        {
+            Console.WriteLine("InitializeWebView");
+            await webView.EnsureCoreWebView2Async(null);
+            Console.WriteLine("WebView2 Runtime Version: " + webView.CoreWebView2.Environment.BrowserVersionString);
+            
+            if (webView?.CoreWebView2 == null)
+            {
+                Console.WriteLine("Not Ready...");
+            }
+            else
+            {
+                webView.CoreWebView2.FaviconChanged += CoreWebView2OnFaviconChanged;
+                webView.CoreWebView2.SourceChanged += CoreWebView2OnSourceChanged;
+                UpdateUrlView();
+            }
+        }
+
+        private void CoreWebView2OnSourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
+        {
+            UpdateUrlView();
+        }
+
+        private void ParseArguments()
+        {
+            var argumentsParsed = ParseArguments(_arguments);
+            if (argumentsParsed == null)
+            {
+                Text = Resources.DefaultTitle;
+                webView.Source = new Uri("https://duckduckgo.com");
+            }
+
+            if (argumentsParsed?.Item1 != null)
+                Text = argumentsParsed.Value.Item1;
+            if (argumentsParsed?.Item2 != null)
+                webView.Source = new Uri(argumentsParsed.Value.Item2);
+        }
+        
+        private void CoreWebView2OnFaviconChanged(object sender, object e)
+        {
+            UpdateIcon();
+        }
+
+        private void UpdateUrlView()
+        {
+            webView?.CoreWebView2?.Navigate(webView.Source.AbsoluteUri);
+            UpdateIcon();
+        }
+
+        private void UpdateIcon()
+        {
+            if (!(webView?.CoreWebView2?.FaviconUri.Length > 0)) return;
+            
+            using (var client = new WebClient())
+            {
+                var content = client.DownloadData(webView.CoreWebView2.FaviconUri);
+                using (var stream = new MemoryStream(content))
+                {
+                    Icon = new Icon(stream);
+                }
+            }
+        }
+
+        private (string, string)? ParseArguments(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    return (Resources.DefaultTitle, "https://duckduckgo.com");
+                case 1:
+                {
+                    var tokens = args[0].Split('=');
+                    if (tokens[0].Equals("title"))
+                        return (tokens[1], null);
+                    break;
+                }
+                case 2:
+                {
+                    var title = string.Empty;
+                    var url = string.Empty;
+                    
+                    foreach (var arg in args)
+                    {
+                        var tokens = arg.Split('=');
+                        switch (tokens[0])
+                        {
+                            case "-title":
+                                title = tokens[1];
+                                break;
+                            case "-url":
+                                url = tokens[1];
+                                break;
+                        }
+                    }
+
+                    return (title, url);
+                }
+                default:
+                {
+                    Console.WriteLine(Resources.InvalidArgumentNumberError);
+                    Application.Exit();
+                    break;
+                }
+            }
+
+            return null;
+        }
+    }
+}
